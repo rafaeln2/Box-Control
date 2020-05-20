205a0f607677dad9353bd65984d91963f9edd561
danielfernandow <d5fdasilva@gmail.com>
2020-05-19 21:42:32 -0300

    alter interface

--- BoxControl/src/DAO/PessoaDAO.java
+++ BoxControl/src/DAO/PessoaDAO.java
@@ -7,14 +7,14 @@ import Entity.Pessoa;
 
 public interface PessoaDAO {
 
-	public void createEntity(Connection conn, Pessoa pessoa) throws Exception;
+	public void createEntity(Pessoa pessoa) throws Exception;
 	
-	public void updateEntity(Connection conn, Pessoa pessoa)throws Exception;
+	public void updateEntity( Pessoa pessoa)throws Exception;
 	
-	public Pessoa findEntity(Connection conn, Pessoa pessoa)throws Exception;
+	public Pessoa findEntity(Pessoa pessoa)throws Exception;
 	
-	public void dropEntity(Connection conn, Pessoa pessoa)throws Exception;
+	public void dropEntity(Pessoa pessoa)throws Exception;
 	
-	Collection<Pessoa> list(Connection conn)throws Exception;
+	Collection<Pessoa> list()throws Exception;
 	
 }
