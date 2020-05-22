281ef72eae8ec219f91cd1127d64022d896ec876
danielfernandow <d5fdasilva@gmail.com>
2020-05-22 19:00:42 -0300

    alter metod

--- BoxControl/src/DAO/PessoaDAO.java
+++ BoxControl/src/DAO/PessoaDAO.java
@@ -8,11 +8,11 @@ public interface PessoaDAO {
 
 	public void createEntity(Pessoa pessoa) throws Exception;
 	
-	public void updateEntity( Pessoa pessoa)throws Exception;
+	public void updateEntity(Pessoa pessoa)throws Exception;
 	
-	public Pessoa findEntity(Pessoa pessoa)throws Exception;
+	public Pessoa findEntity(Integer idPessoa)throws Exception;
 	
-	public void dropEntity(Pessoa pessoa)throws Exception;
+	public void dropEntity(Integer idPessoa)throws Exception;
 	
 	Collection<Pessoa> list()throws Exception;
 	
